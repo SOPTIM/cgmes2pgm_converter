a4752b6a6d060eb128e72b56ca95e732c9ccd795
Eduard Fried <eduard.fried@soptim.de>
2025-10-30 14:46:28 +0100

    cleanup transformer formulas
    - move pp (inspired) formulas to separate file
    
    Signed-off-by: Eduard Fried <eduard.fried@soptim.de>

--- src/cgmes2pgm_converter/components/branch/transformer/util/pst_tapchanger_calculation.py
+++ src/cgmes2pgm_converter/components/branch/transformer/util/pst_tapchanger_calculation.py
@@ -13,8 +13,6 @@
 # limitations under the License.
 
 import logging
-from dataclasses import dataclass
-from typing import Literal
 
 import numpy as np
 import pandas as pd
@@ -129,71 +127,6 @@ def _calc_theta_tabular(trafo, tapside):
     return theta
 
 
-def calc_k_tabular_in_phase(
-    trafo,
-    tapside,
-    tapside_rtc,
-):
-    ## RTC
-    steps_rtc = trafo[f"step{tapside_rtc}_rtc"] - trafo[f"neutralStep{tapside_rtc}_rtc"]
-
-    voltage_increment_rtc = trafo[f"stepSize{tapside_rtc}"]
-
-    u_netz1 = trafo["nomU1"]
-    u_netz2 = trafo["nomU2"]
-    u_rated1 = trafo["ratedU1"]
-    u_rated2 = trafo["ratedU2"]
-
-    k = calc_k_tabular_in_phase2(
-        trafo,
-        tapside,
-        u_netz1,
-        u_netz2,
-        u_rated1,
-        u_rated2,
-        voltage_increment_rtc,
-        steps_rtc,
-    )
-
-    return k
-
-
-def calc_k_tabular_in_phase2(
-    trafo,
-    tapside,
-    u_netz1,
-    u_netz2,
-    u_rated1,
-    u_rated2,
-    voltage_increment_rtc,
-    steps_rtc,
-):
-    w0 = (u_rated2 / u_netz2) / (u_rated1 / u_netz1)
-    if tapside == 1:
-        w0 = 1 / w0
-
-    ### PST
-    tc_ratio1 = trafo["tcRatio1"]
-    tc_ratio2 = trafo["tcRatio2"]
-    tc_ratio = 1  ## dummy
-
-    if not np.isnan(tc_ratio1):
-        tc_ratio = tc_ratio1
-        # tc_ratio = 1 / tc_ratio
-    elif not np.isnan(tc_ratio2):
-        tc_ratio = tc_ratio2
-        # tc_ratio = 1 / tc_ratio
-
-    ### RTC
-
-    k_tap = w0 * (1 + (voltage_increment_rtc / 100 * steps_rtc))
-
-    ## COMBINED
-    k = k_tap * tc_ratio
-
-    return k
-
-
 def _calc_theta_linear(trafo, tapside):
     # s = n-n_0
     # α = s * ∂α
@@ -243,46 +176,7 @@ def _calc_theta_symmetrical(trafo, tapside):
     k = np.abs(t)
     theta = -np.angle(t)
 
-    #####
-
-    rated_hv = rated_u1_
-    rated_lv = rated_u2_
-    nom_hv = u_netz1
-    nom_lv = u_netz2
-    if rated_u1_ < rated_u2_:
-        rated_lv = rated_u1_
-        rated_hv = rated_u2_
-        nom_hv = u_netz2
-        nom_lv = u_netz1
-
-    tap_side = "hv" if tapside == 1 else "lv"
-    # tap_side = "lv"
-
-    _rated_hv, _rated_lv, theta1 = _calc_pp_shift(
-        nom_hv,
-        nom_lv,
-        rated_hv,
-        rated_lv,
-        0.0,
-        [
-            TapChanger(
-                tap_type="symmetrical",
-                tap_side=tap_side,
-                tap_pos=steps_current,
-                tap_neutral=steps_neutral,
-                tap_step_percent=voltage_increment,
-                tap_step_degree=0,  # winding_connection_angle * deg_to_rad,
-            )
-        ],
-    )
-    ## better for amp
-    k1 = _calc_pp_ratio(_rated_hv, _rated_lv, u_netz1, u_netz2)
-    k1 = 1 / k1
-
-    #####
-
     return theta, k
-    # return theta1, k1
 
 
 def _calc_theta_k_asymmetrical(trafo, tapside: int):
@@ -328,47 +222,7 @@ def _calc_theta_k_asymmetrical(trafo, tapside: int):
     k = 1 / k
     theta *= -1
 
-    #####
-
-    rated_hv = u_rated1
-    rated_lv = u_rated2
-    nom_hv = u_netz1
-    nom_lv = u_netz2
-    if u_rated1 < u_rated2:
-        rated_lv = u_rated1
-        rated_hv = u_rated2
-        nom_hv = u_netz2
-        nom_lv = u_netz1
-
-    tap_side = "hv" if tapside == 1 else "lv"
-    tap_side = "lv"
-
-    # tap_step *= -1
-    # tap_side = "lv"
-
-    _rated_hv, _rated_lv, theta1 = _calc_pp_shift(
-        nom_hv,
-        nom_lv,
-        rated_hv,
-        rated_lv,
-        0.0,
-        [
-            TapChanger(
-                tap_type="asymmetrical",
-                tap_side=tap_side,
-                tap_pos=steps_current,
-                tap_neutral=steps_neutral,
-                tap_step_percent=voltage_increment,
-                tap_step_degree=winding_connection_angle * deg_to_rad,
-            )
-        ],
-    )
-    k1 = _calc_pp_ratio(_rated_hv, _rated_lv, nom_hv, nom_lv)
-
-    #####
-
     return theta, k
-    # return theta1, k1
 
 
 def calc_theta_k_asymmetrical_in_phase(
@@ -429,79 +283,7 @@ def calc_theta_k_asymmetrical_in_phase(
     k = 1 / k
     theta *= -1
 
-    #####
-
-    rated_hv = u_rated1
-    rated_lv = u_rated2
-    nom_hv = u_netz1
-    nom_lv = u_netz2
-    if u_rated1 < u_rated2:
-        rated_lv = u_rated1
-        rated_hv = u_rated2
-        nom_hv = u_netz2
-        nom_lv = u_netz1
-
-    _rated_hv, _rated_lv, theta1 = _calc_pp_shift(
-        nom_hv,
-        nom_lv,
-        rated_hv,
-        rated_lv,
-        0.0,
-        [
-            TapChanger(
-                tap_type="asymmetrical",
-                tap_side="hv" if tapside_pst == 1 else "lv",
-                tap_pos=steps_pst,
-                tap_neutral=steps_neutral_pst,
-                tap_step_percent=voltage_increment_pst,
-                tap_step_degree=winding_connection_angle * deg_to_rad,
-            ),
-            TapChanger(
-                tap_type="ratio",
-                tap_side="hv" if tapside_rtc == 1 else "lv",
-                tap_pos=steps_rtc,
-                tap_neutral=steps_neutral_rtc,
-                tap_step_percent=voltage_increment_rtc,
-                tap_step_degree=0.0,
-            ),
-        ],
-    )
-    k1 = _calc_pp_ratio(_rated_hv, _rated_lv, nom_hv, nom_lv)
-    k1r = 1 / k1
-
-    _rated_hv2, _rated_lv2, theta2 = _calc_pp_shift(
-        nom_hv,
-        nom_lv,
-        rated_hv,
-        rated_lv,
-        0.0,
-        [
-            TapChanger(
-                tap_type="ratio",
-                tap_side="lv",  # "hv" if tapside_rtc == 1 else "lv",
-                tap_pos=steps_rtc,
-                tap_neutral=steps_neutral_rtc,
-                tap_step_percent=voltage_increment_rtc,
-                tap_step_degree=0.0,
-            ),
-            TapChanger(
-                tap_type="asymmetrical",
-                tap_side="hv",  # "hv" if tapside_pst == 1 else "lv",
-                tap_pos=steps_pst,
-                tap_neutral=steps_neutral_pst,
-                tap_step_percent=voltage_increment_pst,
-                tap_step_degree=winding_connection_angle * deg_to_rad,
-            ),
-        ],
-    )
-    k2 = _calc_pp_ratio(_rated_hv2, _rated_lv2, nom_hv, nom_lv)
-    k2r = 1 / k2
-
-    #####
-
     return theta, k
-    # return theta1, k1 ## better for amp
-    # return theta2, k2r
 
 
 def calc_theta_k_generic(
@@ -570,87 +352,66 @@ def _calc_k_tabular(trafo):
     return k
 
 
-@dataclass
-class TapChanger:
-    tap_type: Literal["ratio", "asymmetrical", "symmetrical"]
-    tap_side: Literal["hv", "lv"]
-    tap_pos: int
-    tap_neutral: int
-    tap_step_percent: float
-    tap_step_degree: float
-
-
-def _calc_pp_shift(
-    nom_hv: float,
-    nom_lv: float,
-    rated_hv: float,
-    rated_lv: float,
-    base_shift: float,  # in rad, from one PST (can't have multiple PSTs on one transformer)
-    tap_changer: list[TapChanger],
+def calc_k_tabular_in_phase(
+    trafo,
+    tapside,
+    tapside_rtc,
 ):
-    shift = base_shift
+    ## RTC
+    steps_rtc = trafo[f"step{tapside_rtc}_rtc"] - trafo[f"neutralStep{tapside_rtc}_rtc"]
 
-    for tap in tap_changer:
-        direction = 1 if tap.tap_side == "hv" else -1
-        tap_diff = tap.tap_pos - tap.tap_neutral
+    voltage_increment_rtc = trafo[f"stepSize{tapside_rtc}"]
 
-        if tap.tap_type == "ratio" or tap.tap_type == "asymmetrical":
-            steps = tap.tap_step_percent * tap_diff / 100
-            angle = tap.tap_step_degree
+    u_netz1 = trafo["nomU1"]
+    u_netz2 = trafo["nomU2"]
+    u_rated1 = trafo["ratedU1"]
+    u_rated2 = trafo["ratedU2"]
 
-            if tap.tap_side == "lv":
-                _rated_u, _shift = _rated_u_steps(direction, rated_lv, steps, angle)
-                rated_lv = _rated_u
-                shift += _shift
-            elif tap.tap_side == "hv":
-                _rated_u, _shift = _rated_u_steps(direction, rated_hv, steps, angle)
-                rated_hv = _rated_u
-                shift += _shift
-            else:
-                logging.warning("Unknown tap changer side: %s", tap.tap_side)
-                continue
-
-        elif tap.tap_type == "symmetrical":
-
-            if tap.tap_side == "lv":
-                shift += 1 * tap_diff * tap.tap_step_degree
-            elif tap.tap_side == "hv":
-                shift -= 1 * tap_diff * tap.tap_step_degree
-
-            ## arcsin(x) ~~ arctan(x) for small x
-            shift2 = (
-                direction
-                * 2
-                # * np.rad2deg(np.arcsin(tap_diff * tap.tap_step_percent / 100 / 2))
-                * np.arcsin(tap_diff * tap.tap_step_percent / 100 / 2)
-            )
-            shift += (
-                direction * 2 * np.arctan(0.5 * tap_diff * tap.tap_step_percent / 100)
-            )
-            wait = 1
-
-        else:
-            ratio = 1.0
-            logging.warning("Unknown tap changer type: %s", tap.tap_type)
-
-    return rated_hv, rated_lv, shift
-
-
-def _rated_u_steps(direction, rated_u, steps_percent, angle):
-    _du = rated_u * steps_percent
-    _rated_u = np.sqrt(
-        (rated_u + _du * np.cos(angle)) ** 2 + (_du * np.sin(angle)) ** 2
+    k = calc_k_tabular_in_phase2(
+        trafo,
+        tapside,
+        u_netz1,
+        u_netz2,
+        u_rated1,
+        u_rated2,
+        voltage_increment_rtc,
+        steps_rtc,
     )
-    _shift = np.arctan(
-        direction * _du * np.sin(angle) / (rated_u + _du * np.cos(angle))
-    )
-    _shift2 = np.arctan2(
-        direction * _du * np.sin(angle), (rated_u + _du * np.cos(angle))
-    )
-    return _rated_u, _shift
+
+    return k
 
 
-def _calc_pp_ratio(rated_hv, rated_lv, nom_hv, nom_lv):
-    tap_ratio = rated_hv / rated_lv
-    nom_ratio = nom_hv / nom_lv
-    return tap_ratio / nom_ratio
+def calc_k_tabular_in_phase2(
+    trafo,
+    tapside,
+    u_netz1,
+    u_netz2,
+    u_rated1,
+    u_rated2,
+    voltage_increment_rtc,
+    steps_rtc,
+):
+    w0 = (u_rated2 / u_netz2) / (u_rated1 / u_netz1)
+    if tapside == 1:
+        w0 = 1 / w0
+
+    ### PST
+    tc_ratio1 = trafo["tcRatio1"]
+    tc_ratio2 = trafo["tcRatio2"]
+    tc_ratio = 1  ## dummy
+
+    if not np.isnan(tc_ratio1):
+        tc_ratio = tc_ratio1
+        # tc_ratio = 1 / tc_ratio
+    elif not np.isnan(tc_ratio2):
+        tc_ratio = tc_ratio2
+        # tc_ratio = 1 / tc_ratio
+
+    ### RTC
+
+    k_tap = w0 * (1 + (voltage_increment_rtc / 100 * steps_rtc))
+
+    ## COMBINED
+    k = k_tap * tc_ratio
+
+    return k
